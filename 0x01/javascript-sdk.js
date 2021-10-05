61dac44890bca73e69f8afce3b779e056a5fb517
1270 <1270@holbertonschool.com>
2021-10-04 21:06:19 -0500

    Exploring

---
+++ 0x01/javascript-sdk.js
@@ -0,0 +1,8 @@
+console.log("works!");
+/* import Formio from 'formiojs/Formio';
+
+// Load a form
+const formio = new Formio('https://myproject.form.io/myform');
+formio.loadForm().then(function(formio) {
+  console.log("formio: ", formio);
+}); */
